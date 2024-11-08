0e33076c8ab0a7b55ecbf3acee8f6884802562fe
Damon Fedorick <95268795+Ddemon26@users.noreply.github.com>
2024-11-07 23:50:15 -0800

    Update: Readme and Docs

--- TCS GitWizard/Runtime/GitManagement/GitWizardConfig.cs
+++ TCS GitWizard/Runtime/GitManagement/GitWizardConfig.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 namespace TCS.GitWizard {
+    [HelpURL("https://github.com/Ddemon26/TCS-GitWizard/blob/main/README.md")]
     [CreateAssetMenu(menuName = "TCS/GitWizard/Create GitWizardConfig", fileName = "GitWizardConfig", order = 1000)]
     public class GitWizardConfig : ScriptableObject {
         [Header("Installer Title")]
